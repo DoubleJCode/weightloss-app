5deca4217ee0a22ef07cb4aadd2a2be3043c1210
juli8347 <juli8347@edu.ucl.dk>
2023-07-04 18:32:38 +0200

    App project scaffolding from template

---
+++ WeightManagement/WeightManagement.App/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace WeightManagement.App;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
