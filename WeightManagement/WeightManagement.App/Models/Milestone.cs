5cd4ebe7a1c446d56d29800e64d16f78e1de56a9
juli8347 <juli8347@edu.ucl.dk>
2023-07-05 14:59:19 +0200

    Created models

---
+++ WeightManagement/WeightManagement.App/Models/Milestone.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeightManagement.App.Models
+{
+    public class Milestone
+    {
+        public DateTime Date { get; set; }
+        public float Weight { get; set; }
+        public float Bmi { get; set; }
+    }
+}
---
+++ WeightManagement/WeightManagement.App/Models/WeightEntry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeightManagement.App.Models
+{
+    public class WeightEntry
+    {
+        public DateTime Date { get; set; }
+        public float Weight { get; set; }
+    }
+}
